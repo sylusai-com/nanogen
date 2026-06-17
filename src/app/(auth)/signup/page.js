9bc6bdd03e9bfbb9ded96d7323b7f9cd49c43f92
Aman Bhatt <amanbhatt0910@gmail.com>
2026-06-17 09:35:44 +0530

    placeholder updated

--- src/app/(auth)/signup/page.js
+++ src/app/(auth)/signup/page.js
@@ -80,7 +80,7 @@ export default function SignupPage() {
             id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            placeholder="Aman Bhatt"
+            placeholder="Full Name"
             leftIcon={<User className="h-4 w-4" />}
             autoComplete="name"
             required
